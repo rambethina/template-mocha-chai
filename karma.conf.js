985657d14e80229afe1901c889d8b1336eba44e2
vrbethi <ram.bethina@gmail.com>
2016-07-03 07:46:46 -0500

    template for chai mocha

--- karma.conf.js
+++ karma.conf.js
@@ -1,91 +1,64 @@
 // Karma configuration
-'use strict'
+// Generated on Sun Jul 03 2016 07:19:52 GMT-0500 (CDT)
 
-module.exports = function (config) {
-  let customBrowsers = ['Chrome', 'Safari', 'Firefox', 'PhantomJS']
-  if (process.env.TRAVIS) {
-    customBrowsers = ['PhantomJS']
-  }
-
+module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'sinon-chai', 'browserify'],
+    frameworks: ['mocha', 'requirejs'],
+
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.js',
-      'test/*.js'
+      'test-main.js',
+      {pattern: 'src/**/*.js', included: false},
+      {pattern: 'test/**/*Spec.js', included: false}
     ],
 
+
     // list of files to exclude
-    exclude: [],
+    exclude: [
+    ],
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js': ['browserify'],
-      'test/**/*.spec.js': ['browserify']
     },
 
-    // Browserify configuration
-    // The coverage command goes here instead of the preprocessor because we need it to work with browserify
-    browserify: {
-      debug: true,
-      transform: [
-        [
-          'babelify',
-          {
-            presets: 'es2015'
-          }
-        ], [
-          'browserify-istanbul',
-          {
-            instrumenterConfig: {
-              embedSource: true
-            }
-          }
-        ]
-      ]
-    },
-
-    // optionally, configure the reporter
-    // text displays it within the console (alternative: text-summary)
-    // lcov creates a codecov compatible report
-    coverageReporter: {
-      reporters: [
-        {'type': 'text'},
-        {'type': 'html', dir: 'coverage'},
-        {'type': 'lcov'}
-      ]
-    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // coverage is from karma-coverage and provides Istanbul code coverage reports
-    reporters: ['mocha', 'coverage'],
+    reporters: ['progress'],
+
 
     // web server port
     port: 9876,
 
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
+
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
+
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: customBrowsers,
+    browsers: ['Chrome', 'Firefox', 'Safari', 'ChromeCanary'],
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
